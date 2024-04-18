05b73712f1bb44c174190c9f37e7f80f12ddeb96
BotExxo456 <reinisch.lucas070300@gmail.com>
2024-04-18 19:45:13 +0200

    Ternary

--- Ternary Assignment/Program.cs
+++ Ternary Assignment/Program.cs
@@ -6,9 +6,12 @@
         string? answer = Console.ReadLine();
         int answerAsInt;
         bool isInt = Int32.TryParse(answer, out answerAsInt);
-        answer = isInt == false ? "Not a valid Temperature" : answerAsInt < 15 ? "it is too cold here" : answerAsInt > 16 && answerAsInt < 28 
+        answer = isInt == false ? "Not a valid Temperature" : answerAsInt <= 15 ? "it is too cold here" : answerAsInt >= 16 && answerAsInt < 28 
         ? "it is ok" : "it is hot here";
         Console.WriteLine(answer);
         Console.Read();
+
+        
+
     }
 }
