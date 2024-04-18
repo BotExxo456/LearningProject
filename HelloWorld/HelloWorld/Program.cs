ea6be426f98f0d2fc5d91d12aa67aa4513461b79
BotExxo456 <reinisch.lucas070300@gmail.com>
2024-04-18 12:07:16 +0200

    Test

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Lucas!");
-Console.ReadLine();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Lucas!");
+        Console.ReadLine();
+    }
+}
